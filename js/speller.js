ff8a146cf1bf12930473d22613ba87462be75884
Carlos Estrada <cestr008@gmail.com>
2023-04-17 22:07:41 -0400

    refactor code

--- js/speller.js
+++ js/speller.js
@@ -17,36 +17,75 @@ async function loadPeriodicTable() {
   }
 }
 
-function check(inputWord) {
-  if (inputWord.length == 0) {
-    return [];
+function findCandidates(inputWord) {
+  const oneLetterSymbols = [];
+  const twoLetterSymbols = [];
+
+  for (let i = 0; i < inputWord.length; i++) {
+    const oneLetter = inputWord[i];
+
+    // Collect all one letter symbols
+    if (oneLetterSymbols.includes(oneLetter)) continue;
+    if (oneLetter in symbols) {
+      oneLetterSymbols.push(oneLetter);
+    }
+
+    if (i > inputWord.length - 2) continue;
+
+    // Collect all two letter symbols
+    const twoLetter = inputWord.slice(i, i + 2);
+
+    if (twoLetterSymbols.includes(twoLetter)) continue;
+    if (twoLetter in symbols) {
+      twoLetterSymbols.push(twoLetter);
+    }
   }
 
-  // check every element for a symbol matching the next
-  // 1-2 characters of the input word
-  for (let element of elements) {
-    let symbol = element.symbol.toLowerCase();
-    if (symbol.length <= inputWord.length) {
-      if (inputWord.slice(0, symbol.length) == symbol) {
-        // more characters in the input word to try
-        // to match to symbols?
-        if (inputWord.length > symbol.length) {
-          // recurse to check the remainder of the
-          // input word
-          let res = check(inputWord.slice(symbol.length));
+  return [...twoLetterSymbols, ...oneLetterSymbols];
+}
 
-          // was the check successful?
-          if (res.length > 0) {
-            return [symbol, ...res];
-          }
-        } else {
-          return [symbol];
+function spellWord(candidates, charsLeft) {
+  // Base Case
+  if (charsLeft.length === 0) return [];
+
+  // Recursive Case
+  if (charsLeft.length >= 2) {
+    const two = charsLeft.slice(0, 2);
+    const rest = charsLeft.slice(2);
+
+    if (candidates.includes(two)) {
+      if (rest.length > 0) {
+        const result = [two, ...spellWord(candidates, rest)];
+        if (result.join('') === charsLeft) {
+          return result;
         }
+      } else {
+        return [two];
       }
     }
   }
 
-  return [];
+  // Now check for one letter symbols
+  if (charsLeft.length >= 1) {
+    const one = charsLeft[0];
+    const rest = charsLeft.slice(1);
+
+    if (candidates.includes(one)) {
+      if (rest.length > 0) {
+        const result = [one, ...spellWord(candidates, rest)];
+        if (result.join('') === charsLeft) {
+          return result;
+        }
+      } else {
+        return [one];
+      }
+    }
+  }
+}
+
+function check(inputWord) {
+  const candidates = findCandidates(inputWord);
+  return spellWord(candidates, inputWord);
 }
 
 function lookup(elementSymbol) {
